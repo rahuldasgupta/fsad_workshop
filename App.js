97336d106b829b58cfa1d604a5c6bfab90928530
rahuldasgupta <rahul_dasgupta@outlook.com>
2023-04-29 09:38:02 +0530

    Add all my files

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, Image } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+  }
+  render() {
+    return (
+      <View style={{backgroundColor: "#7540ee", flex: 1}}>
+        <View style={{marginTop: 60, justifyContent:"space-between", flexDirection:"row"}}>
+          <MaterialIcons name="arrow-back-ios" style={{marginLeft: 15}} size={24} color="white" />
+          <Text style={{color:"white", fontSize: 24,}}>Sign Up</Text>
+          <FontAwesome name="navicon" style={{marginRight: 15}} size={25} color="white" />
+          
+        </View>
+        <Text style={{fontSize: 30, color:"white", textAlign:"center", marginTop: 80}}>What is your name?</Text>
+        <Image
+          style={{height: 320, width: 320, alignSelf:"center"}}
+          source={{uri: "https://i.ibb.co/0Qct3vS/Conference.png"}}
+        />
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
